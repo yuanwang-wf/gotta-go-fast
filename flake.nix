242174152ad560ce96c023fb38f4af33c8738d1b
yuanwang <yuan.wang@workiva.com>
2023-04-03 08:43:00 -0600

    add flake

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+    flake-root.url = "github:srid/flake-root";
+    mission-control.url = "github:Platonic-Systems/mission-control";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      # systems = [ "x86_64-linux" ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.haskell-flake.flakeModule
+        inputs.flake-root.flakeModule
+        inputs.mission-control.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem = { self', lib, config, pkgs, ... }: {
+        haskellProjects.main = {
+          devShell = {
+            tools = hp:
+              {
+                treefmt = config.treefmt.build.wrapper;
+              } // config.treefmt.build.programs;
+            hlsCheck.enable = true;
+          };
+        };
+        # pre-commit.settings.hooks = {
+        #   nixpkgs-fmt.enable = true;
+        #   cabal-fmt.enable = true;
+        #   fourmolu.enable = true;
+        # };
+        treefmt.config = {
+          inherit (config.flake-root) projectRootFile;
+          package = pkgs.treefmt;
+          flakeFormatter =
+            false; # For https://github.com/numtide/treefmt-nix/issues/55
+
+          programs.ormolu.enable = true;
+          programs.nixpkgs-fmt.enable = true;
+          programs.cabal-fmt.enable = true;
+          programs.hlint.enable = true;
+
+          # We use fourmolu
+          programs.ormolu.package = pkgs.haskellPackages.fourmolu;
+          settings.formatter.ormolu = {
+            options = [ "--ghc-opt" "-XImportQualifiedPost" ];
+          };
+        };
+        mission-control.scripts = {
+          docs = {
+            description = "Start Hoogle server for project dependencies";
+            exec = ''
+              echo http://127.0.0.1:8888
+              hoogle serve -p 8888 --local
+            '';
+            category = "Dev Tools";
+          };
+          fmt = {
+            description = "Format the source tree";
+            exec = config.treefmt.build.wrapper;
+            category = "Dev Tools";
+          };
+          repl = {
+            description = "Start the cabal repl";
+            exec = ''
+              cabal repl "$@"
+            '';
+            category = "Dev Tools";
+          };
+          preview = { exec = "warp -d ${config.packages.draftContent}"; };
+        };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ config.mission-control.devShell self'.devShells.main ];
+        };
+        packages.default = config.packages.blogContent;
+      };
+    };
+}
